2410bb96189b0af5bf90bb78d39e89c4e82c2cff
Karan Khiani <karankhiani1109@gmail.com>
2018-08-14 22:40:45 +0530

    hello

---
+++ server.js
@@ -0,0 +1,46 @@
+var express = require('express')
+var bodyParser =require('body-parser')
+var app = express()
+var http =require('http').Server(app)
+var io = require('socket.io')(http)
+var mongoose = require('mongoose')
+
+app.use(express.static(__dirname))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: false}))
+
+var dbUrl= 'mongodb://admin:karan123@ds221271.mlab.com:21271/newprojectkk'
+
+var Message = mongoose.model('Message',{
+	name: String,
+	message: String
+})
+
+app.get('/messages',(req,res)=> {
+	Message.find({},(err,messages)=>{
+		res.send(messages)
+	})
+	
+})
+app.post('/messages',(req,res)=> {
+	var message = new Message(req.body)
+	message.save((err)=>{
+		if(err)
+			sendStatus(500)
+
+		
+		io.emit('message',req.body)
+		res.sendStatus(200)
+	})
+	
+})
+io.on('connection', (socket) => {
+	console.log('a user connected')
+
+})
+mongoose.connect(dbUrl,(err)=> {
+	console.log('mongo db connection', err)
+})
+var server=http.listen(3000, () => {
+	console.log('server is listening to port',server.address().port)
+})
